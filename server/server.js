2ca6e3f2566d8211918186eb5767225283415282
Andrymar02 <andreamarranca@gmail.com>
2026-06-28 18:06:31 +0200

    fine

--- server/server.js
+++ server/server.js
@@ -2,9 +2,16 @@ const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
 const cors = require('cors');
+const path = require('path');
 
 const app = express();
 app.use(cors());
+
+app.use(express.static(path.join(__dirname, 'dist')));
+app.get(/.*/, (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
+
 const server = http.createServer(app);
 
 const io = new Server(server, {
